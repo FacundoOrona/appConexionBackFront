03ccc7e72d7e775c9ddc4dd541db0cbe845b5183
FacundoOrona <fcndorona@gmail.com>
2025-04-10 14:44:43 -0300

    Implementacion e instalacion de boostrap

--- front-ConexionBackend/src/Aplicacion.jsx
+++ front-ConexionBackend/src/Aplicacion.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Aplicacion = () => {
   return (
-    <div>Aplicacion</div>
+    <>
+    <div className="container mt-5">
+      <h1 className="text-primary">¡Hola React con Bootstrap!</h1>
+      <button className="btn btn-success">Haz clic</button>
+    </div> 
+    </>
   )
 }
