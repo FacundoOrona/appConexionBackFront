d73389d32b777d34bcce5e1260afe9f17230a45b
FacundoOrona <fcndorona@gmail.com>
2025-04-10 15:44:19 -0300

    Creacion de backend con Springboot

---
+++ backendConexion/src/main/java/com/AplicacionConexionFront/backendConexion/BackendConexionApplication.java
@@ -0,0 +1,13 @@
+package com.AplicacionConexionFront.backendConexion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConexionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendConexionApplication.class, args);
+	}
+
+}
---
+++ backendConexion/src/test/java/com/AplicacionConexionFront/backendConexion/BackendConexionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AplicacionConexionFront.backendConexion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendConexionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
